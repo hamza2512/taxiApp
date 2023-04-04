17d3d26260e0576c05330a87bc989ddda67ec84e
Nauman <faheembuildout@gmail.com>
2023-04-04 13:23:20 +0500

    Play store updates package and check is in place

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import TabNavigator from './src/navigation/TabNavigator';
 import {store} from './Redux/Store';
 import {Context} from './src/components/Context';
 import {useKeepAwake} from 'expo-keep-awake';
+import VersionCheck from 'react-native-version-check-expo';
 
 export default function App() {
   // useEffect(() => {
@@ -17,6 +18,19 @@ export default function App() {
   //     deactivateKeepAwake();
   //   };
   // }, []);
+  useEffect(() => {
+    VersionCheck.needUpdate({
+      depth: 2,
+      currentVersion: '1.0',
+      latestVersion: '2.0',
+    }).then((res) => {
+      console.log(res);
+      if (res?.isNeeded) {
+        alert('Please update your Application');
+      }
+      // false; because first two fields of current and the latest versions are the same as "0.1".
+    });
+  }, []);
   useKeepAwake();
   return (
     <Provider store={store}>
